6b7035dc9bd30748bb96c4fb33b90aabb46d400c
hidroinfo <hidroinfo@hidroinfo-HP-ProBook-4440s>
2021-11-15 10:00:46 -0500

    Rama mezclada

--- Parcial/src/ejercicio_parcial.java
+++ Parcial/src/ejercicio_parcial.java
@@ -3,8 +3,8 @@ public class ejercicio_parcial {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.print("Rama Master");
-
+		System.out.print("Rama desarrollador Lara");
+		System.out.print("Rama mezclada");
 	}
 
 }
